2fcfa4ab41abef807283e4a7a25fbe0d336bfe16
Lili Deng <lildeng@microsoft.com>
2021-07-15 15:17:18 +0800

    Not use exact kernel-devel package.

--- Testscripts/Linux/SetupRDMA.sh
+++ Testscripts/Linux/SetupRDMA.sh
@@ -116,7 +116,7 @@ function Main() {
 			LogMsg "Starting RDMA setup for RHEL/CentOS"
 			# required dependencies
 			grep 7.5 /etc/redhat-release || grep 7.6 /etc/redhat-release && curl https://partnerpipelineshare.blob.core.windows.net/kernel-devel-rpms/CentOS-Vault.repo > /etc/yum.repos.d/CentOS-Vault.repo
-			req_pkg="kernel-devel-$(uname -r) redhat-rpm-config rpm-build gcc gcc-gfortran libdb-devel gcc-c++ glibc-devel zlib-devel numactl numactl-devel binutils-devel iptables-devel libstdc++-devel libselinux-devel elfutils-devel libtool java libstdc++.i686 gtk2 atk cairo tcl tk createrepo byacc.x86_64 net-tools tcsh"
+			req_pkg="kernel-devel redhat-rpm-config rpm-build gcc gcc-gfortran libdb-devel gcc-c++ glibc-devel zlib-devel numactl numactl-devel binutils-devel iptables-devel libstdc++-devel libselinux-devel elfutils-devel libtool java libstdc++.i686 gtk2 atk cairo tcl tk createrepo byacc.x86_64 net-tools tcsh"
 			install_package $req_pkg
 			LogMsg "$?: Installed required packages $req_pkg"
 			if [[ ! $(grep 7.8 /etc/redhat-release) ]]; then
