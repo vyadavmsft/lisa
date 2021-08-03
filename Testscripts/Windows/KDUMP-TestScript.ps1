8dbcbe2c13a394d4b37c6c3c4b654e21b5d92eb5
LiliDeng <lildeng@microsoft.com>
2021-08-03 21:49:52 +0800

    Update KDUMP-TestScript.ps1

--- Testscripts/Windows/KDUMP-TestScript.ps1
+++ Testscripts/Windows/KDUMP-TestScript.ps1
@@ -185,16 +185,16 @@ function Main {
         if ($vcpu -eq 4){
             Write-LogInfo "Kdump will be triggered on VCPU 3 of 4"
             Run-LinuxCmd -username $VMUserName -password $VMPassword -ip $Ipv4 -port $VMPort `
-                -command "taskset -c 2 echo c > /proc/sysrq-trigger" -RunInBackGround -runAsSudo | Out-Null
+                -command "taskset -c 0 echo c > /proc/sysrq-trigger" -RunInBackGround -runAsSudo | Out-Null
         } elseif ($crashedVcpu) {
             Write-LogInfo "Kdump will be triggered on $crashedVcpu"
             Run-LinuxCmd -username $VMUserName -password $VMPassword -ip $Ipv4 -port $VMPort `
-                -command "taskset -c $crashedVcpu echo c > /proc/sysrq-trigger" -RunInBackGround -runAsSudo | Out-Null
+                -command "taskset -c 0 echo c > /proc/sysrq-trigger" -RunInBackGround -runAsSudo | Out-Null
         } else {
             # If directly use plink to trigger kdump, command fails to exit, so use start-process
             Write-LogInfo "Set /proc/sysrq-trigger"
             Run-LinuxCmd -username $VMUserName -password $VMPassword -ip $Ipv4 -port $VMPort `
-                -command "sync; echo c > /proc/sysrq-trigger" -RunInBackGround -runAsSudo | Out-Null
+                -command "sync; taskset -c 0 echo c > /proc/sysrq-trigger" -RunInBackGround -runAsSudo | Out-Null
         }
     }
 
