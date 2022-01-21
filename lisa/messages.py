f40bc49644750af554d739e8a0d2cbc9f286b5a6
Lili Deng <lildeng@microsoft.com>
2022-01-21 14:32:26 +0800

    Move common fields into PerMessage class.

--- lisa/messages.py
+++ lisa/messages.py
@@ -2,9 +2,11 @@ from dataclasses import dataclass
 from datetime import datetime
 from decimal import Decimal
 from enum import Enum
-from typing import List, Optional
+from typing import Dict, List, Optional, TypeVar
 
-from lisa.util import constants
+from lisa.util import constants, dict_to_fields
+
+T = TypeVar("T")
 
 
 @dataclass
@@ -35,9 +37,32 @@ class TestRunMessage(MessageBase):
     message: str = ""
 
 
+class NetworkProtocol(str, Enum):
+    IPv4 = "IPv4"
+    IPv6 = "IPv6"
+
+
+class TransportProtocol(str, Enum):
+    Tcp = "TCP"
+    Udp = "UDP"
+
+
 @dataclass
 class PerfMessage(MessageBase):
     type: str = "Performance"
+    test_case_name: str = ""
+    platform: str = ""
+    location: str = ""
+    host_version: str = ""
+    guest_os_type: str = "Linux"
+    distro_version: str = ""
+    vmsize: str = ""
+    kernel_version: str = ""
+    lis_version: str = ""
+    ip_version: str = NetworkProtocol.IPv4
+    protocol_type: str = TransportProtocol.Tcp
+    data_path: str = ""
+    test_date: datetime = datetime.utcnow()
 
 
 DiskSetupType = Enum(
@@ -61,15 +86,6 @@ DiskType = Enum(
 @dataclass
 class DiskPerformanceMessage(PerfMessage):
     tool: str = constants.DISK_PERFORMANCE_TOOL
-    test_case_name: str = ""
-    platform: str = ""
-    location: str = ""
-    host_version: str = ""
-    guest_os_type: str = "Linux"
-    distro_version: str = ""
-    vmsize: str = ""
-    kernel_version: str = ""
-    lis_version: str = ""
     disk_setup_type: DiskSetupType = DiskSetupType.raw
     block_size: int = 0
     disk_type: DiskType = DiskType.nvme
@@ -78,7 +94,6 @@ class DiskPerformanceMessage(PerfMessage):
     qdepth: int = 0
     iodepth: int = 0
     numjob: int = 0
-    test_date: datetime = datetime.utcnow()
     read_iops: Decimal = Decimal(0)
     read_lat_usec: Decimal = Decimal(0)
     randread_iops: Decimal = Decimal(0)
@@ -91,19 +106,6 @@ class DiskPerformanceMessage(PerfMessage):
 
 @dataclass
 class NetworkLatencyPerformanceMessage(PerfMessage):
-    test_case_name: str = ""
-    platform: str = ""
-    location: str = ""
-    host_version: str = ""
-    guest_os_type: str = "Linux"
-    distro_version: str = ""
-    vmsize: str = ""
-    kernel_version: str = ""
-    lis_version: str = ""
-    ip_version: str = "IPv4"
-    protocol_type: str = "TCP"
-    data_path: str = ""
-    test_date: datetime = datetime.utcnow()
     max_latency_us: Decimal = Decimal(0)
     average_latency_us: Decimal = Decimal(0)
     min_latency_us: Decimal = Decimal(0)
@@ -115,19 +117,7 @@ class NetworkLatencyPerformanceMessage(PerfMessage):
 
 @dataclass
 class NetworkPPSPerformanceMessage(PerfMessage):
-    test_execution_tag: str = ""
-    platform: str = ""
-    location: str = ""
-    host_version: str = ""
-    guest_os_type: str = "Linux"
-    distro_version: str = ""
-    vmsize: str = ""
-    kernel_version: str = ""
-    ip_version: str = "IPv4"
-    protocol_type: str = "TCP"
-    data_path: str = ""
     test_type: str = ""
-    test_date: datetime = datetime.utcnow()
     rx_pps_minimum: Decimal = Decimal(0)
     rx_pps_average: Decimal = Decimal(0)
     rx_pps_maximum: Decimal = Decimal(0)
@@ -137,3 +127,54 @@ class NetworkPPSPerformanceMessage(PerfMessage):
     rx_tx_pps_minimum: Decimal = Decimal(0)
     rx_tx_pps_average: Decimal = Decimal(0)
     rx_tx_pps_maximum: Decimal = Decimal(0)
+
+
+@dataclass
+class NetworkNtttcpTCPPerformanceMessage(PerfMessage):
+    connections_num: int = 0
+    throughput_in_gbps: Decimal = Decimal(0)
+    latency_us: Decimal = Decimal(0)
+    buffer_size: Decimal = Decimal(0)
+    tx_packets: Decimal = Decimal(0)
+    rx_packets: Decimal = Decimal(0)
+    pkts_interrupts: Decimal = Decimal(0)
+    number_of_receivers: int = 1
+    number_of_senders: int = 1
+    sender_cycles_per_byte: Decimal = Decimal(0)
+    connections_created_time: int = 0
+    retrans_segments: int = 0
+    receiver_cycles_rer_byte: Decimal = Decimal(0)
+
+
+@dataclass
+class NetworkNtttcpUDPPerformanceMessage(PerfMessage):
+    connections_num: int = 0
+    number_of_receivers: int = 1
+    number_of_senders: int = 1
+    connections_created_time: int = 0
+    receiver_cycles_rer_byte: Decimal = Decimal(0)
+    send_buffer_size: Decimal = Decimal(0)
+    tx_throughput_in_gbps: Decimal = Decimal(0)
+    rx_throughput_in_gbps: Decimal = Decimal(0)
+    data_loss: Decimal = Decimal(0)
+    packet_size_kbytes: Decimal = Decimal(0)
+
+
+@dataclass
+class NetworkIperfSingleTCPPerformanceMessage(PerfMessage):
+    buffer_size_bytes: Decimal = Decimal(0)
+    tx_throughput_in_gbps: Decimal = Decimal(0)
+    rx_throughput_in_gbps: Decimal = Decimal(0)
+    retransmitted_segments: Decimal = Decimal(0)
+    congestion_windowsize_kb: Decimal = Decimal(0)
+
+
+@dataclass
+class NetworkIperfUDPPerformanceMessage(NetworkNtttcpUDPPerformanceMessage):
+    pass
+
+
+def create_message_list(messages: List[T], information: Dict[str, str]) -> List[T]:
+    for message in messages:
+        dict_to_fields(information, message)
+    return messages
